b722c006188b35995ff218dcb3783e3714f8e4ce
ozkartadev <ozkartadev@gmail.com>
2017-11-21 01:59:17 +0400

    validators are included in routers

--- api/v1/components/shared/routes.js
+++ api/v1/components/shared/routes.js
@@ -1,5 +1,6 @@
 module.exports = function (express, passport) {
   let router = express.Router();
+  let ObjectValidator = require('../../shared/utils/object-validator');
 
   router.get('/test', (req, res) => {
     res.status(200).json({});
--- api/v1/components/visitor/routes.js
+++ api/v1/components/visitor/routes.js
@@ -3,30 +3,7 @@ module.exports = function (express, passport) {
   let ObjectValidator = require('../../shared/utils/object-validator');
 
   router.get('/test', (req, res) => {
-
-    let obj = {
-      fName: 'ozkarta',
-      lName: '   ',
-      age: '25'
-    };
-    let validator = {
-      fName: {
-        required: true,
-        minLength: 15,
-        maxLength: 3,
-        type: 'string'
-      },
-      age: {
-        required: true,
-        minValue: 26,
-        maxValue: 24,
-        type: 'number'
-      },
-      lName: {
-        required: true
-      }
-    };
-    res.status(200).json(ObjectValidator.validateObject(obj, validator));
+    res.status(200).json({});
   });
 
   return router;
